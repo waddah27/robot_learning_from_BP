4bed7076f9cb704da2b5f8f7775aacbe68f6b62a
waddah27 <waddahkh.ali94@gmail.com>
2026-01-01 23:05:55 +0300

    Fully implemented and integrated scalple geometry with handler on the robot's end-endfector

--- mjModeling/base.py
+++ mjModeling/base.py
@@ -7,6 +7,8 @@ scalpelHadnler1_dirname = "scalpel_model/scalpel/scalpelHandler1.STL"
 scalpelHandler1_path = os.path.join(base_dir, scalpelHadnler1_dirname)
 scalpelHadnler2_dirname = "scalpel_model/scalpel/scalpelHandler2.STL"
 scalpelHandler2_path = os.path.join(base_dir, scalpelHadnler2_dirname)
+scalpel_dirname = "scalpel_model/scalpel/Scalpel.stl"
+scalpel_path = os.path.join(base_dir, scalpel_dirname)
 class Robot:
     def __init__(self):
         self._model = None
@@ -31,8 +33,12 @@ class Robot:
         mesh2 = spec.add_mesh(name="mesh2", file=scalpelHandler2_path)
         mesh2.refquat = [0.0, 1.0, 0, 0] # Match the orientation of part 1
         mesh2.scale = [0.001, 0.001, 0.001]
-
-        # 3. Setup Body Hierarchy
+        # 3. Register the scalpel mesh
+        mesh3 = spec.add_mesh(name="mesh3", file=scalpel_path)
+        #  Rotate 60 degrees To mach scalpel being perpendicular to ee  
+        mesh3.refquat = [0, 0.5, 0, 0.866] 
+        mesh3.scale = [0.0001, 0.0001, 0.0001]
+        # 4. Setup Body Hierarchy
         ee_body = spec.body("link7")
         attach_site = spec.site("attachment_site")
         
@@ -41,26 +47,34 @@ class Robot:
         handler.pos =  attach_site.pos
         handler.quat = attach_site.quat
         handler_depth = -0.09
-        # 4. Add Part 1 (First half of handler)
+        # 5. Add Part 1 (First half of handler)
         handler.add_geom(
-            name="handler_part1_visual", 
+            name="handler_part1_geom", 
             type=mujoco.mjtGeom.mjGEOM_MESH, 
             meshname="mesh1", 
             rgba=[0.9, 0.9, 0.0, 1], # Orange
             pos = attach_site.pos + [-0.045 ,0.045, handler_depth]
         )
 
-        # 5. Part 2 (Second half of handler)
+        # 6. Part 2 (Second half of handler)
         # We add this to the SAME body so they are fixed together.
         handler.add_geom(
-            name="handler_part2_visual", 
+            name="handler_part2_geom", 
             type=mujoco.mjtGeom.mjGEOM_MESH, 
             meshname="mesh2", 
             rgba=[0.8, 0.8, 0.8, 1], # Grey to distinguish
             pos=attach_site.pos + [-0.0175, -0.002, 0.025] 
         )
+        # 7. Scalpel geometry from rigestered scalpel mesh
+        handler.add_geom(
+            name="Scalpel_geom", 
+            type=mujoco.mjtGeom.mjGEOM_MESH, 
+            meshname="mesh3", 
+            rgba=[0.8, 0.8, 0.8, 1], # Grey to distinguish
+            pos=attach_site.pos #+ [-0.0175, -0.002, 0.025] 
+        )
 
-        # 6. Compile
+        # 8. Compile model, forward it with data
         self._model = spec.compile()
         self._data = mujoco.MjData(self._model)
         mujoco.mj_forward(self._model, self._data)
