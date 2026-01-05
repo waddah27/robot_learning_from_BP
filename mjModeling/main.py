ccbc607e31104e87016dc209ed6ed969b474a8e5
waddah27 <waddahkh.ali94@gmail.com>
2026-01-05 16:38:31 +0300

    PEP8 refactored main.py

--- mjModeling/main.py
+++ mjModeling/main.py
@@ -1,34 +1,13 @@
-from mjModeling import *
+from mjModeling import robot_scene_xml
 from kuka_iiwa_14.iiwa14_model import iiwa14
 from visualization.visualizer import Visualize
 from kinematics import JacobianIK, quat_to_mat
 
 # 1 - build experiment env
 robot = iiwa14().create(robot_scene_xml)
-  
 print(robot.model.opt.gravity)
- 
 # 2 - simulator
 visualizer = Visualize(robot)
 
-if __name__=='__main__':
-  
-    
-    # Rendering 
-  if CONF["run_one_frame"]:
-      with mujoco.Renderer(robot.model) as renderer:
-          # first we do forward kins to compute the accelerations (derivatives of the state)
-          mujoco.mj_forward(robot.model, robot.data)
-          renderer.update_scene(robot.data)
-          image_array = renderer.render()
-          # img = Image.fromarray(image_array)
-          # img.show()
-          plt.imshow(image_array)
-          plt.axis("off")
-          plt.title("Robot")
-          plt.show()
-  else:
+if __name__ == '__main__':
     visualizer.run()
-  
-
-
